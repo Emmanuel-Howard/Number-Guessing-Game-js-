da96ec6f83bf9292c750d41262deee81e8456cfe
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-21 15:34:52 -0500

    Defining Constants

---
+++ index.js
@@ -0,0 +1,10 @@
+// Defining Constants
+const guessBox = document.getElementById("guessBox");
+const guessBtn = document.getElementById("guessBtn");
+const userGuess = document.getElementById("userGuess");
+const hint = document.getElementById("hint");
+
+// On Click function
+guessBtn.onclick.function(){
+    
+}
