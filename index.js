a98b05a69c3f94fdd5844991c8a6dcfcd3c1fa1a
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-21 15:47:38 -0500

    Create Random Number

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const guessBox = document.getElementById("guessBox");
 const guessBtn = document.getElementById("guessBtn");
 const userGuess = document.getElementById("userGuess");
 const hint = document.getElementById("hint");
+const min = 1;
+const max = 100;
+const randomNum = Math.floor(Math.random() * max) + min;
+console.log(randomNum);
 
 // On Click function
-guessBtn.onclick.function(){
-    
-}
+guessBtn.onclick = function() {
+    userGuess.textContent = `Your guess is ${guessBox.value}.`;
+    console.log(guessBox.value);
+};
+
+// Random Number Function
